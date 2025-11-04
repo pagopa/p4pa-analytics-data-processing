64f72327dae81d6b20e9552c5386abae3bdb6290
antonio.torre <antonio.torre@pagopa.it>
2025-11-04 16:25:32 +0100

    empty ms

---
+++ src/main/java/it/gov/pagopa/analytics/process/AnalyticsDataProcessingApplication.java
@@ -0,0 +1,14 @@
+package it.gov.pagopa.analytics.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+public class AnalyticsDataProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsDataProcessingApplication.class, args);
+	}
+
+}
