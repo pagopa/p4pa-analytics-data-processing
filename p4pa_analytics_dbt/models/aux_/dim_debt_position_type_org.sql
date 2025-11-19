0261922a85b5a76a9fd8f0db3f611fa9301ac14a
MatteoCorti <matteo.corti@quantyca.it>
2025-11-19 15:40:49 +0100

    feat: P4ADEV-4173 create debt_position_type_org models

---
+++ p4pa_analytics_dbt/models/aux_/dim_debt_position_type_org.sql
@@ -0,0 +1,22 @@
+{{ config(
+    materialized = "incremental",
+    unique_key = "debt_position_type_org_pk",
+    incremental_strategy = "merge",
+) }}
+
+
+select
+    md5(debt_position_type_org_id::varchar) as debt_position_type_org_pk,
+    debt_position_type_org_id,
+    debt_position_type_id,
+    organization_id,
+    code,
+    description,
+    flag_spontaneous,
+    -- technical fields
+    current_timestamp as processed_time
+from {{ ref("debt_position_type_orgs") }}
+
+{% if is_incremental() %}
+    where processed_time >= (select coalesce(max(processed_time), '1900-01-01') from {{ this }} )
+{% endif %}
