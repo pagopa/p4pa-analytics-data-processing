5388f5cc44cd1fea13d11fc511b1dabc9f20768c
MatteoCorti <matteo.corti@quantyca.it>
2025-11-28 11:12:58 +0100

    feature: P4ADEV-4174 add stg to dwh layer models

---
+++ p4pa_analytics_dbt/models/dwh/dim_office.sql
@@ -0,0 +1,42 @@
+{{ config(
+    materialized = "incremental",
+    unique_key = "office_pk",
+    incremental_strategy = "merge",
+) }}
+
+with base as (
+    select
+        office_code,
+        debt_position_type_org_id,
+        CAST(TO_CHAR(date_receipt, 'YYYY') AS INT) AS operating_year,
+        office_description,
+        md5(coalesce(office_description, '')) as hash_checksum,
+        processed_time
+    from {{ ref('payment_assessment_detail') }}
+),
+
+source as (
+    select distinct
+    {{ dbt_utils.generate_surrogate_key(['office_code', 'debt_position_type_org_id', 'operating_year']) }} as office_pk,
+    office_code,
+    debt_position_type_org_id,
+    operating_year,
+    office_description,
+    hash_checksum
+    from base as b
+
+    {% if is_incremental() %}
+        where b.processed_time >= (select coalesce(max(processed_time), '1900-01-01') from {{ this }} )
+    {% endif %}
+),
+
+new_data as (
+    select s.*
+    from source s
+    left join {{ this }} t on
+        s.office_pk = t.office_pk
+    where s.hash_checksum <> coalesce(t.hash_checksum, '')
+)
+
+select *, current_timestamp as processed_time
+from new_data
