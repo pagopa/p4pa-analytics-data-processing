f117bc794b95c6dd1ff177c713474dbfc468ed12
antonio.torre <antonio.torre@pagopa.it>
2026-03-05 19:38:11 +0100

    fix: Fix CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,14 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   java
-  id("org.springframework.boot") version "4.0.0"
+  id("org.springframework.boot") version "4.0.3"
   id("io.spring.dependency-management") version "1.1.7"
   jacoco
-  id("org.sonarqube") version "7.2.1.6560"
+  id("org.sonarqube") version "7.2.3.7755"
   id("com.github.ben-manes.versions") version "0.53.0"
-  id("org.openapi.generator") version "7.17.0"
-  id("com.gorylenko.gradle-git-properties") version "2.5.4"
-  id("com.github.jk1.dependency-license-report") version "3.0.1"
+  id("org.openapi.generator") version "7.20.0"
+  id("com.gorylenko.gradle-git-properties") version "2.5.7"
+  id("com.github.jk1.dependency-license-report") version "3.1.1"
 }
 
 group = "it.gov.pagopa.payhub"
@@ -47,20 +47,24 @@ repositories {
   mavenCentral()
 }
 
-val springDocOpenApiVersion = "3.0.0"
+val springDocOpenApiVersion = "3.0.2"
 val janinoVersion = "3.1.12"
-val openApiToolsVersion = "0.2.8"
-val micrometerVersion = "1.6.1"
-val otelVersion = "1.57.0"
+val openApiToolsVersion = "0.2.9"
+val micrometerVersion = "1.6.3"
 val bouncycastleVersion = "1.83"
-val httpClientVersion = "5.5.1"
+val httpClientVersion = "5.6"
 val commonsLang3Version = "3.20.0"
-val temporalVersion = "1.32.1"
-val protobufJavaVersion = "4.33.2"
-val grpcBomVersion = "1.77.0"
+val temporalVersion = "1.33.0"
+val protobufJavaVersion = "4.34.0"
+val grpcBomVersion = "1.79.0"
 val guavaVersion = "33.5.0-jre"
+val otelVersion = "1.59.0"
 val mapStructVersion = "1.6.3"
 val podamVersion = "8.0.2.RELEASE"
+
+// fix cve
+val jackson2CoreVersion = "2.21.1"
+val jackson3CoreVersion = "3.1.0"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webmvc")
@@ -94,6 +98,10 @@ dependencies {
   implementation(platform("io.grpc:grpc-bom:${grpcBomVersion}"))
   implementation("com.google.guava:guava:$guavaVersion")
   implementation("io.opentelemetry:opentelemetry-opentracing-shim:${otelVersion}")
+
+  // CVE fix
+  implementation("tools.jackson.core:jackson-core:${jackson3CoreVersion}")
+  implementation("com.fasterxml.jackson.core:jackson-core:${jackson2CoreVersion}")
 
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
