c49bc90a6e6bad21dc3c7e4c90ee69afb5a9ce58
antonio.torre <antonio.torre@pagopa.it>
2026-08-11 22:11:00 +0200

    fix: Returning field error details

--- src/main/java/it/gov/pagopa/analytics/process/exception/common/InvalidValueException.java
+++ src/main/java/it/gov/pagopa/analytics/process/exception/common/InvalidValueException.java
@@ -1,10 +1,12 @@
 package it.gov.pagopa.analytics.process.exception.common;
 
 import it.gov.pagopa.analytics.process.dto.generated.ErrorFieldDTO;
+import it.gov.pagopa.analytics.process.exception.NotRetryableActivityException;
 
 import java.util.List;
 
-public class InvalidValueException extends BaseBusinessException {
+@SuppressWarnings("java:S110") // Suppress "Inheritance tree of classes should not be too deep": allowed for exception hierarchy
+public class InvalidValueException extends NotRetryableActivityException {
 
   public InvalidValueException(String code, String message) {
     this(code, message, null, null);
